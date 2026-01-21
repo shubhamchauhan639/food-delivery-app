180d53b785afe5eb2a755b9e4950b5c00636a992
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-01-21 19:08:07 +0530

    search resturants complete and bug fixed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,50 +1,63 @@
 import Card from "./Card"
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmers";
-const Body = ()=> {
+const Body = () => {
   const [listOfRes, setListOfRes] = useState([])
+  const [filterRest , setFilterRes] = useState([])
+  const [searchText , setSearchText] = useState("")
   // let resList = 
   // console.log("resObj:", resList);
   // console.log("resObj[1]:", resList[0]);
-  useEffect(()=>{
+  useEffect(() => {
     fetchData()
-  },[])
-  
-  const fetchData =async ()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.765495&lng=77.19021219999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-      const json = await data.json();
- const restaurants =
-    json?.data?.cards?.find(
-      (c) => c?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    )?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+  }, [])
 
-  setListOfRes(restaurants);
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.765495&lng=77.19021219999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    const restaurants =
+      json?.data?.cards?.find(
+        (c) => c?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      )?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+
+    setListOfRes(restaurants);
+    setFilterRes(restaurants)
   }
-  if(listOfRes.length === 0){
-    return <Shimmer/>
+  if (listOfRes.length === 0) {
+    return <Shimmer />
   }
-  
-  
-return(
-  <div className="body">
-    <div className="filter">
-      <div className="search">
-        <input type="text" className="search-box" />
-        <button>Search</button>
-      </div>
-    <button className="filter-button" onClick={()=>{
-    const FilterList = listOfRes.filter(
-      (res) => res.info.avgRating > 4
+
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input type="text" className="search-box" value={searchText}  onChange={(e)=> {setSearchText(e.target.value)}}/>
+          <button onClick={()=>{
+             console.log(searchText)
+             const filteredRes = listOfRes.filter((res)=>res?.info?.name
+        ?.toLowerCase()
+        .includes(searchText.toLowerCase())
     );
-    setListOfRes(FilterList)
-    // console.log(listOfRes)
-  }}>Top Rated restaurant</button>
+    setFilterRes(filteredRes)
+          }
+            
+           
+          }>Search</button>
+        </div>
+        <button className="filter-button" onClick={() => {
+          const FilterList = listOfRes.filter(
+            (res) => res.info.avgRating > 4
+          );
+          setListOfRes(FilterList)
+          // console.log(listOfRes)
+        }}>Top Rated restaurant</button>
+      </div>
+      <div className="container">
+
+        {filterRest.map((restaurant) => (<Card key={restaurant.info.id} resData={restaurant.info} />))}
+      </div>
     </div>
-    <div className="container">
-      
-    {listOfRes.map((restaurant)=>(<Card key = {restaurant.info.id} resData = {restaurant.info}/>))}
-  </div>
-  </div>
-)
+  )
 }
 export default Body;
