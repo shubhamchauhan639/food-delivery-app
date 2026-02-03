f3f6740159d0c6dac3bf647c9facefc61399dff0
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-02-03 23:55:33 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = ()=>{
   <ul>
     <li><Link to = "/">Home</Link></li>
     <li><Link to ="/about">Help</Link></li>
-    <li>Offers</li>
+    <li><Link to= "/offers">Offers</Link></li>
     <li>cart</li>
     <button className="login-btn" onClick={()=> {
       loginButton === "Log In" 
