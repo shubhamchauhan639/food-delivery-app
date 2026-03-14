a7b74d9c72400cba32f236ffd77784c8388dfbdf
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-03-14 21:59:09 +0530

    promoted label card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -47,14 +47,17 @@ const Card = ({ resData }) => {
     </div>
   );
 };
-const withPromotedLabel = (Card)=>{
-  return()=>{
-   < div>
-      <label>Promoted</label>
-    <Card/>
-    </div>
- 
-  }
-}
+const withPromotedLabel = (Card) => {
+  return (props) => {
+    return (
+      <div className="relative">
+        <label className="absolute top-2 left-2 bg-red-500 text-white text-xs px-2 py-1 rounded z-10">
+          Promoted
+        </label>
+        <Card {...props} />
+      </div>
+    );
+  };
+};
 
 export default Card;
